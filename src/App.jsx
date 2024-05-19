b468d0fe778fbdc00f48a667585c07f9cf17f2c0
Daryl Bacurin <darylbacurin14@gmail.com>
2024-05-19 22:18:03 +0800

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from "react";
 import Search from "./components/Search/Search";
 import FoodList from "./components/FoodList/FoodList";
 import Nav from "./components/Nav/Nav";
+import Container from "./components/Container/Container";
+import InnerContainer from "./components/InnerContainer/InnerContainer";
 import "./App.css";
+import FoodDetails from "./components/FoodDetails/FoodDetails";
+
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("656329");
   return (
     <>
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} setFoodId={setFoodId} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+      </Container>
     </>
   );
 }
--- src/components/FoodList/FoodList.jsx
+++ src/components/FoodList/FoodList.jsx
@@ -1,10 +1,10 @@
 import FoodItem from "../FoodItem/FoodItem";
 
-export default function FoodList({ foodData }) {
+export default function FoodList({ foodData, setFoodId }) {
   return (
     <div>
       {foodData.map((food) => (
-        <FoodItem key={food.id} food={food} />
+        <FoodItem key={food.id} food={food} setFoodId={setFoodId} />
       ))}
     </div>
   );
